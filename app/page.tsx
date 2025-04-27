49f94c6dad876182a73c9d72082e43bc3c5fad27
Ryan Smith <rysmith2113@gmail.com>
2025-04-27 05:07:36 -0400

    Remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -8,31 +8,18 @@
 
 import { JSX } from "react";
 
-import GenCodeBlock from "@/components/gen-code-block";
 import InputBlock from "@/components/input-block";
 import Title from "@/components/title";
 
 export default function Home(): JSX.Element {
 	return (
 		<div className="flex flex-col justify-center items-center h-screen min-h-screen px-4 sm:px-8 md:mx-48">
-			<div className="absolute top-10">
-				<Title/>
+			<div className="absolute top-10 mt-10">
+				<Title title="YiqiAI" subtitle="Your stocks • All the data • Powered by AI"/>
 			</div>
 			
 			<main className="w-full flex flex-col">
-				<div className="flex flex-col w-full gap-y-2 h-full">
-					<InputBlock/>
-
-					{/* <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 w-full h-auto md:h-[500px]">
-						<div className="h-full">
-							<GenCodeBlock/>
-						</div>
-
-						<div className="bg-default-50 w-full rounded-md">
-							Filler
-						</div>
-					</div> */}
-				</div>
+				<InputBlock/>
 			</main>
 		</div>
 	);
